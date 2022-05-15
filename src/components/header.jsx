e682da1e39f585747f1646961fce671d96e8ccec
shiwook_github_01 <shiwook_github_01@hotmail.com>
2022-05-16 03:02:20 +0900

    empty section injected

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,8 +2,8 @@ import { StyledHeader } from './styles/Header.styled.js';
 export default function header (){
   return (
       <StyledHeader>
-        <h1 id='slide-show' data-testid='slide-show'></h1>
-        <h1 id='slide-show-controller' data-testid='slide-show-controller'></h1>
+        <h1 id='slide-show' data-testid='slide-show'>hellow</h1>
+        <h1 id='slide-show-controller' data-testid='slide-show-controller'>hellow2</h1>
       </StyledHeader>
   )
 }
