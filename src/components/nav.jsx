4e215f2ea52819157ac0fe2822ac80dc2f71aa22
shiwook_github_01 <shiwook_github_01@hotmail.com>
2022-05-16 02:51:04 +0900

    i got error from deploying section and deleted menu click function.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,11 +1,5 @@
 import { StyledNav } from './styles/Nav.styled.js';
 
-function menuOnClick() {
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-}
-
 export default function nav (){
   return (
   <>
