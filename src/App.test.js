8a4c33bf3bd292cb6a39b1b46e0f7c141f177fd3
shiwook_github_01 <shiwook_github_01@hotmail.com>
2022-05-16 02:22:54 +0900

    check header CICD again

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/theme/i);
+  const linkElement = screen.getAllByAltText(/Header/i);
   expect(linkElement).toBeInTheDocument();
 });
